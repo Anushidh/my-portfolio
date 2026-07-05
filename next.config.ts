8e721ed7c4c3dc28f9bfca316a10e3113e58b865
Anushidh <anushidh101@gmail.com>
2026-07-05 16:23:55 +0530

    disable vercel toolbar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    vercelToolbar: false,
+  },
 };
 
 export default nextConfig;
